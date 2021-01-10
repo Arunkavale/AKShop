a7f0f345e8ce74383acc35ce43f397ad8ed8f87d
Arun Kavale <arun.k@cloudbloom.io>
2021-01-10 09:26:04 +0530

    added loader and Message toaster

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,7 +6,9 @@ import axios from 'axios';
 export const listProducts = () => async (dispatch) => {
     try {
         dispatch({type: PRODUCT_LIST_REQUEST});
+
         const {data} = await axios.get('/api/products');
+        
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
             payload: data
